0349c346c84d5bb47f35c2f527f81440074cf23e
Lucas Alves <138324870+tluccas@users.noreply.github.com>
2025-09-24 15:29:56 -0300

    first commit

---
+++ src/main/java/com/alvesdev/Desafio/Cnab/DesafioCnabApplication.java
@@ -0,0 +1,13 @@
+package com.alvesdev.Desafio.Cnab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCnabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCnabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvesdev/Desafio/Cnab/DesafioCnabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvesdev.Desafio.Cnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCnabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
