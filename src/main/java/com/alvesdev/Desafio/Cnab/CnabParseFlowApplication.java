88bed31c2bd0a1f4a8191ebe5c9c49ec23720b90
Lucas Alves <138324870+tluccas@users.noreply.github.com>
2025-09-25 19:51:39 -0300

    docs(CnabParseFlowApplication): add documentação Swagger em CnabParseFlowApplication.java

--- src/main/java/com/alvesdev/Desafio/Cnab/CnabParseFlowApplication.java
+++ src/main/java/com/alvesdev/Desafio/Cnab/CnabParseFlowApplication.java
@@ -1,9 +1,12 @@
 package com.alvesdev.Desafio.Cnab;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Cnab ParseFlow", version = "1", description = "A project designed to solve a challenge, with the intention of putting my knowledge into practice"))
 public class CnabParseFlowApplication {
 
 	public static void main(String[] args) {
